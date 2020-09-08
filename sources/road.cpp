81dae80f3e1e732fdc510c5cc1752fc12bcacb97
mbourand <mbourand@student.42.fr>
2020-09-08 22:49:04 +0200

    road

--- sources/road.cpp
+++ sources/road.cpp
@@ -1,4 +1,4 @@
-#include "road.h"
+#include "utils.h"
 
 /*
 **	Si il y a un fichier, les points sont parsés depuis ce dernier
@@ -31,6 +31,7 @@ Road::Road(int nb_cars, std::string filename)
 bool Road::parse_points(std::ifstream& file)
 {
 	std::string line;
+
 	for (int wall = 0; !file.eof() && wall < 2; wall++)
 	{
 		if (!std::getline(file, line))
@@ -39,9 +40,11 @@ bool Road::parse_points(std::ifstream& file)
 				wall.clear();
 			return false;
 		}
+
 		for (int i = 0; line[i]; i++)
 		{
 			std::array<int, 2> coords;
+
 			for (int j = 0; j < coords.size(); j++)
 			{
 				if (!std::isdigit(line[i]))
@@ -50,47 +53,72 @@ bool Road::parse_points(std::ifstream& file)
 						wall.clear();
 					return false;
 				}
-				coords[j] = std::stoi(line.substr(i, line.size()));
+
+				coords[j] = std::stof(line.substr(i, line.size()));
 				i += line.substr(i, line.size()).size() + 1;
 			}
-			wall_points[wall].push_back(sf::Vector2i(coords[0], coords[1]));
+
+			wall_points[wall].push_back(sf::Vector2f(coords[0], coords[1]));
 		}
 	}
 	return true;
 }
 
-void Road::update_drawing()
+void Road::update_drawing(sf::RenderWindow& window)
 {
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 	{
-		if (distanceSq(sf::Mouse::getPosition(), finish[wall]) < FINISH_CURSOR_TRIGGER)
+		if (distanceSq(window.mapPixelToCoords(sf::Mouse::getPosition()), finish[wall]) < FINISH_CURSOR_TRIGGER)
 			wall++;
 		else
-			wall_points[wall].push_back(sf::Mouse::getPosition());
+			wall_points[wall].push_back(window.mapPixelToCoords(sf::Mouse::getPosition()));
 	}
 
 	if (wall == 2)
 		state = LEARNING;
 }
 
-void Road::update_learning()
+void Road::update_learning(sf::RenderWindow& window)
 {
 
+}
+
+void Road::draw_learning(sf::RenderWindow& window)
+{
+
+}
+
+void Road::draw_drawing(sf::RenderWindow& window)
+{
+	sf::RectangleShape rs;
+	rs.setFillColor(sf::Color::White);
+
+	sf::Vector2i mouse = sf::Mouse::getPosition();
+
+	for (auto wall : wall_points)
+	{
+		for (int i = 1; i < wall.size(); i++)
+		{
+			window.draw(rs);
+		}
+	}
 }
 
 /*
 **	Lance la fonction d'update correspondant à state
 */
-void Road::update()
+void Road::update(sf::RenderWindow& window)
 {
-	std::map<RoadState, void (Road::*)(void)> funcs { {DRAWING, &Road::update_drawing}, {LEARNING, &Road::update_learning} };
+	std::map<RoadState, void (Road::*)(sf::RenderWindow&)> funcs { {DRAWING, &Road::update_drawing}, {LEARNING, &Road::update_learning} };
 	auto pair = funcs.find(state);
-	std::invoke(pair->second, *this);
+	std::invoke(pair->second, *this, window);
 }
 
 void Road::draw(sf::RenderWindow& window)
 {
-
+	std::map<RoadState, void (Road::*)(sf::RenderWindow&)> funcs { {DRAWING, &Road::draw_drawing}, {LEARNING, &Road::draw_learning} };
+	auto pair = funcs.find(state);
+	std::invoke(pair->second, *this, window);
 }
 
 void Road::restart()
